e2cb218a5349a50eb04d6a4da8a42970ab9b6b3b
Andrey Merezhkin <andrei122.98@mail.ru>
2025-11-24 20:07:11 +0200

    Исправил недочеты в уроке "Автоматизация раздела Store"

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,6 +1,9 @@
 import allure
 import pytest
 import requests
+import jsonschema
+from .schemas.store_schema import STORE_SCHEMA
+from .schemas.store_schema import INVENTORY_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -13,6 +16,7 @@ class TestStore:
                 "id": 1,
                 "petId": 1,
                 "quantity": 1,
+                "shipDate": "2025-11-04T10:05:49.431Z",
                 "status": "placed",
                 "complete": True
             }
@@ -23,45 +27,39 @@ class TestStore:
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 200
-
-        with allure.step("Проверка параметров питомца в ответе"):
-            assert response_json['id'] == payload['id'], "id заказ не совпадает с ожидаемым"
-            assert response_json['petId'] == payload['petId'], "petId заказа не совпадает с ожидаемым"
-            assert response_json['quantity'] == payload['quantity'], "quantity заказов не совпадает с ожидаемым"
-            assert response_json['status'] == payload['status'], "status заказа не совпадает с ожидаемым"
-            assert response_json['complete'] == payload['complete'], "complete не совпадает с ожидаемым"
+            jsonschema.validate(response.json(), STORE_SCHEMA)
 
     @allure.title("Получение информации о заказе по ID")
-    def test_get_order_by_id(self):
+    def test_get_order_by_id(self, create_order):
         with allure.step("Получение ID созданного заказа"):
-            order_id = 1
+            order_id = create_order["id"]
 
         with allure.step("Отправка запроса на получение информации о заказе по ID"):
-            response = requests.get(f"{BASE_URL}/store/order/1")
+            response = requests.get(f"{BASE_URL}/store/order/{order_id}")
 
         with allure.step("Проверка статуса ответа и данных заказа"):
             assert response.status_code == 200
             assert response.json()["id"] == order_id
 
     @allure.title("Удаление заказа по ID")
-    def test_delete_order_by_id(self):
+    def test_delete_order_by_id(self, create_order):
         with allure.step("Получение ID созданного заказа"):
-            order_id = 1
+            order_id = create_order["id"]
 
         with allure.step("Отправка запроса на удаление заказа"):
-            delete_response = requests.delete(f"{BASE_URL}/store/order/1")
+            delete_response = requests.delete(f"{BASE_URL}/store/order/{order_id}")
 
         with allure.step("Проверка успешного удаления"):
             assert delete_response.status_code == 200
 
         with allure.step("Отправка запроса на получение информации о заказе по ID"):
-            response = requests.get(f"{BASE_URL}/store/order/1")
+            response = requests.get(f"{BASE_URL}/store/order/{order_id}")
 
         with allure.step("Проверка статуса ответа и данных заказа"):
             assert response.status_code == 404
 
         with allure.step("Проверка что заказ действительно удален"):
-            get_response_after_delete = requests.get(f"{BASE_URL}/store/order/1")
+            get_response_after_delete = requests.get(f"{BASE_URL}/store/order/{order_id}")
             assert get_response_after_delete.status_code == 404
 
     @allure.title("Попытка получить информацию о несуществующем заказе")
@@ -82,17 +80,4 @@ class TestStore:
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
-
-        with allure.step("Проверка что ответ содержит данные инвентаря в правильном формате"):
-            #assert response.json() == {"approved": 57, "delivered": 50}
-            inventory_data = response.json()
-            assert "approved" in inventory_data
-            assert "delivered" in inventory_data
-
-        with allure.step("Проверка типов данных и положительных значений"):
-            assert isinstance(inventory_data["approved"], int) and inventory_data["approved"] >= 0
-            assert isinstance(inventory_data["delivered"], int) and inventory_data["delivered"] >= 0
-
-
-
-
+            jsonschema.validate(response.json(), INVENTORY_SCHEMA)
